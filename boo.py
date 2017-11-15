A first unit test that failed: ql with jit returns wrong result. Fixed by removing @jit

boo.py
@@ -130,7 +130,7 @@ def boo_product(qlm1, qlm2, prod):
     prod[0] *= 4*np.pi/(2*l+1)
     
     
-@jit(nopython=True)
+#@jit(nopython=True) #Results are wrong with jit!
 def ql(qlm):
     """Second order rotational invariant of the bond orientational order of l-fold symmetry
     $$ q_\ell = \sqrt{\frac{4\pi}{2l+1} \sum_{m=-\ell}^{\ell} |q_{\ell m}|^2 } $$"""