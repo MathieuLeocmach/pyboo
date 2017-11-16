63b799c11aad16b3f77918d39835672d6c35b7da
Mathieu Leocmach <mathieu.leocmach@univ-lyon1.fr>
2017-11-16 15:56:45 +0100

    test q4 values

--- boo/test_boo.py
+++ boo/test_boo.py
@@ -1,6 +1,5 @@
 import pytest
-from boo import *
-
+from .boo import *
 from math import sqrt
 
 golden = (1 + sqrt(5))/2
@@ -13,10 +12,12 @@ pos_ico = np.array([ [0,0,0],
 bonds_ico = np.column_stack((np.zeros(12, int), np.arange(12)+1))
 
 def test_ico():
-    qlm = bonds2qlm(pos_ico, bonds_ico)
-    assert ql(qlm)[0] == pytest.approx(0.66332495807107972)
-    assert wl(qlm)[0] == pytest.approx(-0.052131352806275739)
-    assert ql(qlm)[0] == pytest.approx(sqrt(product(qlm,qlm)[0]))
+    q6m = bonds2qlm(pos_ico, bonds_ico)
+    assert ql(q6m)[0] == pytest.approx(0.66332495807107972)
+    assert wl(q6m)[0] == pytest.approx(-0.052131352806275739)
+    assert ql(q6m)[0] == pytest.approx(sqrt(product(q6m,q6m)[0]))
+    q4m = bonds2qlm(pos_ico, bonds_ico, l=4)
+    assert ql(q4m)[0] == pytest.approx(0)
     
 pos_fcc = np.array([[0,0,0],
     [1,1,0], [1,-1,0], [-1,1,0], [-1,-1,0],
@@ -29,3 +30,5 @@ def test_fcc():
     assert ql(qlm)[0] == pytest.approx(0.57452425971406984)
     assert wl(qlm)[0] == pytest.approx(-0.0026260383340077592)
     assert ql(qlm)[0] == pytest.approx(sqrt(product(qlm,qlm)[0]))
+    q4m = bonds2qlm(pos_fcc, bonds_ico, l=4)
+    assert ql(q4m)[0] == pytest.approx(0.19094065395649326)
