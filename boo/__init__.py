dbcecea6651d315543e982c461c35e8deb51ae3e
Mathieu Leocmach <mathieu.leocmach@univ-lyon1.fr>
2017-11-18 15:05:38 +0100

    from .boo import

--- boo/__init__.py
+++ boo/__init__.py
@@ -7,5 +7,5 @@
  
 __version__ = "1.0.0"
  
-from boo import vect2Ylm, single_pos2qlm, bonds2qlm, coarsegrain_qlm, product, ql, wl
-from boo import x_bonds, x_particles, crystallinity, gG_l
+from .boo import vect2Ylm, single_pos2qlm, bonds2qlm, coarsegrain_qlm, product, ql, wl
+from .boo import x_bonds, x_particles, crystallinity, gG_l
