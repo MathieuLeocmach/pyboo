ab710b69fc2785bbb04d08edd9396bd777331a32
Mathieu Leocmach <mathieu.leocmach@univ-lyon1.fr>
2017-11-24 11:52:15 +0100

    Check PEP8 styling with pylint

--- boo/__init__.py
+++ boo/__init__.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
- 
+
 """
-    A Python package to compute bond orientational order parameters as defined by Steinhardt Physical Review B (1983) doi:10.1103/PhysRevB.28.784.
+A Python package to compute bond orientational order parameters
+as defined by Steinhardt Physical Review B (1983)
+doi:10.1103/PhysRevB.28.784.
 """
- 
+
 __version__ = "1.0.0"
- 
+
 from .boo import vect2Ylm, single_pos2qlm, bonds2qlm, ngbs2qlm, coarsegrain_qlm
 from .boo import product, ql, wl
 from .boo import x_bonds, x_ngbs, x_particles, crystallinity, gG_l
